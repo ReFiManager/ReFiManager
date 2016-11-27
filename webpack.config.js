1faac75cba091e8c18f1a1d3927747eb005d1c42
Budry <zarubaondra@gmail.com>
2016-11-27 15:56:35 +0100

    Added basic build script

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+/**
+ * This file is part of the ReFiManager package.
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the license.md file that was distributed with this source code.
+**/
+
+const path = require('path')
+
+module.exports = {
+  entry: path.join(__dirname, 'src/scripts/index.js'),
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: 'refimanager.js'
+  },
+  resolve: {
+    root: [
+      path.resolve(__dirname + '/src')
+    ],
+    extensions: ['', '.js']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'stage-0', 'react']
+        }
+      }
+    ]
+  }
+}
